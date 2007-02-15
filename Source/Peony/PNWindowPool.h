adba43380aaab78da869473c499e8e72d6c42324
Tony Arnold <tony@tonyarnold.com>
2007-02-15 14:57:29 +0000

    Resolves another memory leak.

--- Source/Peony/PNWindowPool.h
+++ Source/Peony/PNWindowPool.h
@@ -1,27 +1,37 @@
-//
-//  PNWindowPool.h
-//  Peony framework
-//
-//  Copyright 2004, Thomas Staller  <playback@users.sourceforge.net>
-//  Copyright 2006-2007, Tony Arnold <tony@tonyarnold.com
-//
-//  See COPYING for licensing details
-//  
+/*
+	PNWindowPool.h
+	See COPYING for licensing details
+	Copyright 2004, Thomas Staller  <playback@users.sourceforge.net>
+	Copyright 2006-2007, Tony Arnold <tony@tonyarnold.com>
+*/
 
+/*! @header       PNWindowPool.h
+    @discussion   This interface provides a method to maintain a shared list of valid, active PNWindow objects that are currently attached to desktops.
+ */
 #import <Foundation/Foundation.h>
 #import "CGSPrivate.h" 
-#import "PNWindow.h" 
+#import "PNWindow.h"
 
-
+/*! @class       PNWindowPool
+    @abstract    Provides a pool of currently allocated, valid PNWindow objects who are assigned to desktops
+    @discussion  This interface provides a method to maintain a shared list of valid, active PNWindow objects that are currently attached to desktops.
+ */
 @interface PNWindowPool : NSObject 
 {
-	NSMutableDictionary*	mWindows; 
+	NSMutableDictionary*	mWindows;
 }
 
-#pragma mark Lifetime 
+/*! @method   sharedWindowPool
+    @abstract Creates (if necessary) and returns a shared instance of the current class.  
+    @result   Returns the newly initialized (if necessary) PNWindowPool object or nil on error.
+*/
 + (id) sharedWindowPool;
 
-#pragma mark Operations  
+/*! @method   windowWithId:   
+    @abstract Creates (if necessary) and returns the PNWindow instance with the window identifier specified.  
+    @param    windowId  
+              A CGSWindow specifying the identifier of the window to return.
+    @result   Returns the newly initialized (if necessary) PNWindow object or nil on error.
+*/
 - (PNWindow*) windowWithId: (CGSWindow) windowId; 
-
 @end
--- Source/Peony/PNWindowPool.m
+++ Source/Peony/PNWindowPool.m
@@ -1,12 +1,9 @@
-//
-//  PNWindowPool.h
-//  Peony framework
-//
-//  Copyright 2004, Thomas Staller  <playback@users.sourceforge.net>
-//  Copyright 2006-2007, Tony Arnold <tony@tonyarnold.com
-//
-//  See COPYING for licensing details
-//  
+/*
+	PNWindowPool.m
+	See COPYING for licensing details
+	Copyright 2004, Thomas Staller  <playback@users.sourceforge.net>
+	Copyright 2006-2007, Tony Arnold <tony@tonyarnold.com>
+ */
 
 #import "PNWindowPool.h"
 #import "PNWindow.h" 
@@ -15,47 +12,47 @@
 @implementation PNWindowPool
 
 #pragma mark -
-#pragma mark Lifetime 
+#pragma mark Initialization and lifecycle
 
-+ (id) sharedWindowPool {
++ (id) sharedWindowPool 
+{
 	static PNWindowPool* ms_oINSTANCE = nil; 
 	
 	if (ms_oINSTANCE == nil)
+  {
 		ms_oINSTANCE = [[PNWindowPool alloc] init];  
-	
+	}
+  
 	return ms_oINSTANCE; 
 }
 
-- (id) init {
-	if (self = [super init]) {
-		// initialize attributes 
-		mWindows = [[NSMutableDictionary dictionary] retain]; 
-		// register observers 
+- (id) init 
+{
+	if (self = [super init]) 
+  {
+		mWindows = [[NSMutableDictionary alloc] init]; 
 		[[NSNotificationCenter defaultCenter] addObserver: self selector: @selector(onWindowRemoved:) name: kPnOnWindowRemoved object: nil]; 
-	
 		return self; 
 	}
 	
 	return nil; 
 }
 
-- (void) dealloc {
-	// unregister 
+- (void) dealloc 
+{
 	[[NSNotificationCenter defaultCenter] removeObserver: self]; 
-	// release attributes 
 	[mWindows release]; 
-	// super...
 	[super dealloc]; 
 }
 
 #pragma mark -
-#pragma mark Operations
+#pragma mark Methods
 
-- (PNWindow*) windowWithId: (CGSWindow) windowId {
-	// create and add it if necessary 
+- (PNWindow*) windowWithId: (CGSWindow) windowId 
+{
+  // If the window is not present in the current pool, create a new instance with the specified ID and add it
 	if ([mWindows objectForKey: [NSNumber numberWithInt: windowId]] == nil) {
-    PNWindow *newWindow = [[PNWindow alloc] initWithWindowId: windowId];
-		[mWindows setObject: newWindow forKey: [NSNumber numberWithInt: windowId]];
+		[mWindows setObject: [[PNWindow alloc] initWithWindowId: windowId] forKey: [NSNumber numberWithInt: windowId]];
 	}
   
 	return [mWindows objectForKey: [NSNumber numberWithInt: windowId]]; 
@@ -64,18 +61,10 @@
 #pragma mark -
 #pragma mark Notification Sinks 
 
-- (void) onWindowRemoved: (NSNotification*) notification {
-	PNWindow* window = [notification object]; 
-		
-#if 0
-	// check if the window is still valid, and if it isn't remove it from the map
-	if ([window isValid])
-		return; 
-#endif 
-	
-	// hmm, now we remove the dead window proxy as it is no longer contained in any 
-	// desktop
-	[mWindows removeObjectForKey: [NSNumber numberWithInt: [window nativeWindow]]]; 
+- (void) onWindowRemoved: (NSNotification*) notification 
+{
+	// Remove the dead window proxy as it is no longer contained in any desktop
+	[mWindows removeObjectForKey: [NSNumber numberWithInt: [(PNWindow*)[notification object] nativeWindow]]];
 }
 
 @end
